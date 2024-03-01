07eebd7a576626f57a3a0fe177c4daec20e294a7
Nate <ntwestern@outlook.com>
2024-02-29 17:09:40 -0700

    gh pages vite config

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,10 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/outsideapp/",
   define: {
     'process.env': process.env
-  },    
+  },
   plugins: [vue()],
   resolve: {
     alias: [
